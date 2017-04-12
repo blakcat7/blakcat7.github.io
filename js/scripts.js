8fb5300f791d804d3993ae244324ddbdea3b938c
Anthea <am921996@gmail.com>
2017-04-13 03:19:30 +0400

    updated-ui-website
    changed front-end to minimal

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,16 @@
 
     jQuery(document).ready(function () {
 
-        
-       /* Preloader */
-		
-        $(window).on('load', function() {
-          $('body').addClass('loaded');
+
+        /* Preloader */
+
+        $(window).on('load', function () {
+            $('body').addClass('loaded');
         });
-		
-		
-		
-       /* Smooth Scroll */
+
+
+
+        /* Smooth Scroll */
 
         $('a.smoth-scroll').on("click", function (e) {
             var anchor = $(this);
@@ -21,12 +21,12 @@
             }, 1000);
             e.preventDefault();
         });
-		
 
 
-       
-       /* Scroll Naviagation Background Change with Sticky Navigation */
-		 
+
+
+        /* Scroll Naviagation Background Change with Sticky Navigation */
+
         $(window).on('scroll', function () {
             if ($(window).scrollTop() > 100) {
                 $('.header-top-area').addClass('navigation-background');
@@ -34,12 +34,12 @@
                 $('.header-top-area').removeClass('navigation-background');
             }
         });
-		
-		
-		
-		
-       /* Mobile Navigation Hide or Collapse on Click */
-		
+
+
+
+
+        /* Mobile Navigation Hide or Collapse on Click */
+
         $(document).on('click', '.navbar-collapse.in', function (e) {
             if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
                 $(this).collapse('hide');
@@ -48,42 +48,42 @@
         $('body').scrollspy({
             target: '.navbar-collapse',
             offset: 195
-        
-		 });
-		 
-		
-		
-		
+
+        });
+
+
+
+
         /* Scroll To Top */
-		
-        $(window).scroll(function(){
-        if ($(this).scrollTop() >= 500) {
-            $('.scroll-to-top').fadeIn();
-         } else {
-            $('.scroll-to-top').fadeOut();
-         }
-	   });
-	
-	
-	    $('.scroll-to-top').click(function(){
-		  $('html, body').animate({scrollTop : 0},800);
-		  return false;
-	    });
-		
-		
-		
-		
+
+        $(window).scroll(function () {
+            if ($(this).scrollTop() >= 500) {
+                $('.scroll-to-top').fadeIn();
+            } else {
+                $('.scroll-to-top').fadeOut();
+            }
+        });
+
+
+        $('.scroll-to-top').click(function () {
+            $('html, body').animate({scrollTop: 0}, 800);
+            return false;
+        });
+
+
+
+
         /* Typed.js */
-		
-        $(window).load(function(){
-        $(".typing").typed({
-            strings: ["I like meows~.", "I am UI/UX Specialist.", "Based in Dubai, UAE."],    /* You can change the home section typing text from
-	                                                                                            here and do not use "&" use "and" */
-            typeSpeed: 50
-          });
-         });
-        
-		 
+        $(window).load(function () {
+            $(".typing").typed({
+                strings: ["Anthea Marie.", "About Me."],
+                typeSpeed: 50,
+                backDelay: 2000,
+                loop: !0,
+            });
+        });
+
+
         /* Parallax Background */
 
         $(window).stellar({
@@ -94,47 +94,47 @@
             verticalOffset: 0,
         });
 
-        
-		
-		
+
+
+
         /* Portfolio Filtering */
 
         $('.portfolio-inner').mixItUp();
 
 
-       
+
         /* Magnific Popup */
 
         $('.portfolio-popup').magnificPopup({
             type: 'image',
-			
-            gallery: { enabled: true },
-			zoom: { enabled: true,
-			        duration: 500
-					
-          },
-		  
-         image:{
-               markup: '<div class="mfp-figure portfolio-pop-up">'+
-               '<div class="mfp-close"></div>'+
-               '<div class="mfp-img"></div>'+
-               '<div class="mfp-bottom-bar portfolio_title">'+
-               '<div class="mfp-title"></div>'+
-               '<div class="mfp-counter"></div>'+
-               '</div>'+
-               '</div>',
 
-               titleSrc:function(item){
-                return item.el.attr('title');
-              }
+            gallery: {enabled: true},
+            zoom: {enabled: true,
+                duration: 500
+
+            },
+
+            image: {
+                markup: '<div class="mfp-figure portfolio-pop-up">' +
+                        '<div class="mfp-close"></div>' +
+                        '<div class="mfp-img"></div>' +
+                        '<div class="mfp-bottom-bar portfolio_title">' +
+                        '<div class="mfp-title"></div>' +
+                        '<div class="mfp-counter"></div>' +
+                        '</div>' +
+                        '</div>',
+
+                titleSrc: function (item) {
+                    return item.el.attr('title');
+                }
             }
-		  
-		  
-          });
 
-       
-	   
-		 
+
+        });
+
+
+
+
         /* Testimonial Carousel/Slider */
 
         $(".testimonial-carousel-list").owlCarousel({
@@ -150,44 +150,44 @@
             itemsMobile: [479, 1],
             autoHeight: true,
             pagination: false,
-            transitionStyle : "backSlide"
+            transitionStyle: "backSlide"
         });
-		
-		
-		
-		
+
+
+
+
         /* Statistics Counter */
-		
-        $('.statistics').appear(function() {
-           var counter = $(this).find('.statistics-count');
-           var toCount = counter.data('count');
-      
-           $(counter).countTo({
-           from: 0,
-           to: toCount,
-           speed: 5000,
-           refreshInterval: 50
-           })
-           });
-		   
-		  
-         
-         /* Google Map */
-		 
-         $('#my-address').gMap({
+
+        $('.statistics').appear(function () {
+            var counter = $(this).find('.statistics-count');
+            var toCount = counter.data('count');
+
+            $(counter).countTo({
+                from: 0,
+                to: toCount,
+                speed: 5000,
+                refreshInterval: 50
+            })
+        });
+
+
+
+        /* Google Map */
+
+        $('#my-address').gMap({
             zoom: 5,
             scrollwheel: true,
             maptype: 'ROADMAP',
-            markers:[
-            {
-            address: "New York",  /* You can change your address from here */
-            html: "<b>Address</b>: <br> Area-2, Rose Area, New York, U.S.A.",   /* You can change display address text from here */
-            popup: true
-            }
+            markers: [
+                {
+                    address: "New York", /* You can change your address from here */
+                    html: "<b>Address</b>: <br> Area-2, Rose Area, New York, U.S.A.", /* You can change display address text from here */
+                    popup: true
+                }
             ]
-            });
-              
-		   
-            });
+        });
 
-   })(jQuery);
+
+    });
+
+})(jQuery);
